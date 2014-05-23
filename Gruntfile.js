6c49743aede83d5491598c836b6c0ddc10effe2e
rexblack <mail@benignware.com>
2014-05-24 00:40:54 +0200

    Added tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,18 +32,10 @@ module.exports = function(grunt) {
     css_wrap: {
       default_options: {
         options: {
+          selector: '.my-app'
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'tmp/default_options': ['test/fixtures/styles.css']
         }
       }
     },
